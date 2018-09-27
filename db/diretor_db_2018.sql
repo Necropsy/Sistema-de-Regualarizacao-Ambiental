35230c4d1a48f9739bf1b7252848703a5c04536d
bcnbruno <b_cnbruno@hotmail.com>
2018-09-27 10:10:12 -0300

    atualização do banco de dados

--- db/diretor_db_2018.sql
+++ db/diretor_db_2018.sql
@@ -7,7 +7,6 @@ CREATE TABLE IF NOT EXISTS `Zona` (
   PRIMARY KEY (`idZona`))
 ENGINE = InnoDB;
 
-
 -- -----------------------------------------------------
 -- Table `Bairro`
 -- -----------------------------------------------------
@@ -17,32 +16,22 @@ CREATE TABLE IF NOT EXISTS `Bairro` (
   PRIMARY KEY (`idBairro`))
 ENGINE = InnoDB;
 
-
 -- -----------------------------------------------------
--- Table `Rua`
+-- Table `Logradouro`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Rua` (
+CREATE TABLE IF NOT EXISTS `Logradouro` (
   `CEP` INT NOT NULL,
-  `nomeRua` VARCHAR(150) NOT NULL,
-  `Logradouro` VARCHAR(45) NOT NULL,
-  `areaMínimaLote` DOUBLE NOT NULL,
-  `testadaMínima` DOUBLE NOT NULL,
-  `taxaDePermeabilizacao` DOUBLE NOT NULL,
-  `taxaDeOcupacaoMax` DOUBLE NOT NULL,
-  `numPavimentos` INT NOT NULL,
-  `larguraCalcadaMin` DOUBLE NOT NULL,
-  `afastFrontalMin` DOUBLE NOT NULL,
-  `coefAproveitamento` DOUBLE NOT NULL,
+  `nomeLogradouro` VARCHAR(150) NOT NULL,
+  `tipoLogradouro` VARCHAR(45) NOT NULL,
   `Bairro_idBairro` INT NOT NULL,
-  PRIMARY KEY (`CEP`),
-  INDEX `fk_Rua_Bairro1_idx` (`Bairro_idBairro` ASC) VISIBLE,
-  CONSTRAINT `fk_Rua_Bairro1`
+  PRIMARY KEY (`CEP`, `Bairro_idBairro`),
+  INDEX `fk_Logradouro_Bairro1_idx` (`Bairro_idBairro` ASC) VISIBLE,
+  CONSTRAINT `fk_Logradouro_Bairro1`
     FOREIGN KEY (`Bairro_idBairro`)
     REFERENCES `mydb`.`Bairro` (`idBairro`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
-
 
 -- -----------------------------------------------------
 -- Table `Zona_has_Bairro`
@@ -65,3 +54,27 @@ CREATE TABLE IF NOT EXISTS `Zona_has_Bairro` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+-- -----------------------------------------------------
+-- Table `Lote`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Lote` (
+  `idLote` INT NOT NULL,
+  `tipoLote` VARCHAR(45) NOT NULL,
+  `areaMínimaLote` DOUBLE NOT NULL,
+  `testadaMínima` DOUBLE NOT NULL,
+  `taxaDePermeabilizacao` DOUBLE NOT NULL,
+  `taxaDeOcupacaoMax` DOUBLE NOT NULL,
+  `numPavimentos` INT NOT NULL,
+  `larguraCalcadaMin` DOUBLE NOT NULL,
+  `afastFrontalMin` DOUBLE NOT NULL,
+  `coefAproveitamento` DOUBLE NOT NULL,
+  `Logradouro_CEP` INT NOT NULL,
+  `Logradouro_Bairro_idBairro` INT NOT NULL,
+  PRIMARY KEY (`idLote`),
+  INDEX `fk_Lote_Logradouro1_idx` (`Logradouro_CEP` ASC, `Logradouro_Bairro_idBairro` ASC) VISIBLE,
+  CONSTRAINT `fk_Lote_Logradouro1`
+    FOREIGN KEY (`Logradouro_CEP` , `Logradouro_Bairro_idBairro`)
+    REFERENCES `mydb`.`Logradouro` (`CEP` , `Bairro_idBairro`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
